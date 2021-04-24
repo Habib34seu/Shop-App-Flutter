2fcbdba2bacf1bed3e28ec4b797629de33eff25a
Habibur Rahman <mohammadhabiburrahman5@gmail.com>
2021-04-25 00:23:53 +0600

    Curd

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
+  final String id;
   final String title;
   final String imageUrl;
 
   UserProductItem(
+    this.id,
     this.title,
     this.imageUrl,
   );
@@ -17,18 +23,23 @@ class UserProductItem extends StatelessWidget {
       ),
       trailing: Container(
         width: 100,
-          child: Row(
+        child: Row(
           children: [
             IconButton(
-              icon: Icon(Icons.edit), 
-              color: Theme.of(context).primaryColor,
-              onPressed: (){}
-            ),
+                icon: Icon(Icons.edit),
+                color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  Navigator.of(context).pushNamed(
+                    EditProductScreen.routeName,
+                    arguments: id,
+                  );
+                }),
             IconButton(
-              icon: Icon(Icons.delete), 
-              color: Theme.of(context).errorColor,
-              onPressed: (){}
-            ),
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () {
+                  Provider.of<Products>(context,listen: false).deleteProduct(id);
+                }),
           ],
         ),
       ),
